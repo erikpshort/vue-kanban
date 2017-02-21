4e148c1a95be9b80c4996ee57a4349f52abefeba
erikpshort <erikshort@hotmail.com>
2017-02-21 15:11:45 -0700

    More Routes

--- server/custom-routes/user-routes.js
+++ server/custom-routes/user-routes.js
@@ -1,5 +1,10 @@
 let Boards = require('../models/board')
 let Lists = require('../models/list')
+let Tasks = require('../models/task')
+let Comments = require('../models/comment')
+let Activities = require('../models/activity')
+let Checklists = require('../models/checklist')
+
 
 export default {
   userBoards: {
@@ -8,8 +13,8 @@ export default {
     method(req, res, next) {
       let action = 'Find User Boards'
       Boards.find({ creatorId: req.session.uid })
-        .then(boards => {
-          res.send(handleResponse(action, boards))
+        .then(data => {
+          res.send(handleResponse(action, data))
         }).catch(error => {
           return next(handleResponse(action, null, error))
         })
@@ -22,14 +27,70 @@ export default {
       let action = 'Find Board Lists'
       console.log(req.params.id)
       Lists.find({ boardId: req.params.id })
-        .then(lists => {
-          res.send(handleResponse(action, lists))
+        .then(data => {
+          res.send(handleResponse(action, data))
         }).catch(error => {
           return next(handleResponse(action, null, error))
         })
     }
+},
+  listTasks: {
+    path: '/lists/:id/tasks',
+    reqType: 'get',
+        method(req, res, next){
+        let action = 'Find Lists Tasks'
+        console.log(req.params.id)
+        Tasks.find({ listId: req.params.id })
+          .then(data => {
+            res.send(handleResponse(action, data))
+          }).catch(error => {
+            return next(handleResponse(action, null, error))
+          })
+      }
+  },
+  taskComments: {
+    path: '/tasks/:id/comments',
+    reqType: 'get',
+        method(req, res, next){
+        let action = 'Find Task Comments'
+        console.log(req.params.id)
+        Comments.find({ taskId: req.params.id })
+          .then(data => {
+            res.send(handleResponse(action, data))
+          }).catch(error => {
+            return next(handleResponse(action, null, error))
+          })
+      }
+  },
+  taskChecklists: {
+    path: '/tasks/:id/Checklists',
+    reqType: 'get',
+        method(req, res, next){
+        let action = 'Find Task Checklists'
+        console.log(req.params.id)
+        Checklists.find({ taskId: req.params.id })
+          .then(data => {
+            res.send(handleResponse(action, data))
+          }).catch(error => {
+            return next(handleResponse(action, null, error))
+          })
+      }
+  },
+  taskActivities: {
+    path: '/tasks/:id/Activities',
+    reqType: 'get',
+        method(req, res, next){
+        let action = 'Find Task Activities'
+        console.log(req.params.id)
+        Activities.find({ taskId: req.params.id })
+          .then(data => {
+            res.send(handleResponse(action, data))
+          }).catch(error => {
+            return next(handleResponse(action, null, error))
+          })
+      }
+  },
 
-  }
 }
 
 
